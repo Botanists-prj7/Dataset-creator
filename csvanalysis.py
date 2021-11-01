ee5e9a81192b202d162166d5bccb804c2f60667f
Frederik Fuglsang Midtgaard <midtgaard@hotmail.dk>
2021-11-01 14:15:15 +0100

    Doing the push again

--- csvanalysis.py
+++ csvanalysis.py
@@ -13,19 +13,22 @@ from shapely import wkt
 from shapely import geometry
 from shapely.geometry.point import Point
 import rtree
-from tqdm import tqdm, tqdm_notebook
+#from tqdm import tqdm, tqdm_notebook
 import time
+import csvTools
 
+#dk_grid_gdf = csvTools.convert_csv_to_gdf('csv_files\\DK_Grid_10000.csv',True,'EPSG:3857')
 
-tqdm.pandas()
+"""
+#tqdm.pandas()
 print('Reading Grid CSV')
-df = pd.read_csv('csv_files/TestDK_Grid_10000.csv')
+df = pd.read_csv('csv_files/DK_Grid_10000.csv')
 print('Setting WKT...')
-df['geometry'] = df['geometry'].apply(wkt.loads).progress_apply(lambda x: x)
+df['geometry'] = df['geometry'].apply(wkt.loads)#.progress_apply(lambda x: x)
 print('Converting projection')
-soil = gpd.GeoDataFrame(df, crs='EPSG:3857').progress_apply(lambda x: x)
+soil = gpd.GeoDataFrame(df, crs='EPSG:3857')#.progress_apply(lambda x: x)
 print('Converting projection...')
-soil = soil.to_crs('EPSG:3857').progress_apply(lambda x: x)
+soil = soil.to_crs('EPSG:3857')#.progress_apply(lambda x: x)
 #print(df)
 
 
@@ -33,16 +36,45 @@ print('Converting plants csv')
 df2 = pd.read_csv('data/data_with_lat_long.csv')
 newdf = df2
 print('Applying geometry conversion for plants')
-gdf = gpd.GeoDataFrame(newdf, geometry=gpd.points_from_xy(df2['decimalLongitude'], df2['decimalLatitude']), crs='EPSG:4326').progress_apply(lambda x: x)
+gdf = gpd.GeoDataFrame(newdf, geometry=gpd.points_from_xy(df2['decimalLongitude'], df2['decimalLatitude']), crs='EPSG:4326')
 print('Converting plants data projection')
-gdf = gdf.to_crs('EPSG:3857').progress_apply(lambda x: x)
+gdf = gdf.to_crs('EPSG:3857')
 print('Setting geometry for plants')
-gdf.set_geometry('geometry').progress_apply(lambda x: x)
+gdf.set_geometry('geometry')
 #print(gdf.head())
 
 print('Finding intersecting points...')
-points_within = gpd.sjoin(gdf, soil, op='within', how='inner').progress_apply(lambda x: x)
-gpd.GeoDataFrame(points_within).to_csv('csv_files/plantsoilintersection2.csv')
+points_within = gpd.sjoin(gdf, soil, predicate='within', how='inner')
+#gpd.GeoDataFrame(points_within).to_csv('csv_files/plantsoilintersection2.csv')
+"""
+
+plants_within_gdf = csvTools.convert_csv_to_gdf('csv_files\\plantsoilintersection2.csv',True,'EPSG:3857')
+#DONE DK_Plant_GDF = DK_GRID
+DK_Plant_gdf = csvTools.convert_csv_to_gdf('csv_files\\DK_Grid_10000.csv',True,'EPSG:3857')
+
+#DONE points_within.drop rows where index_right is None/NA or whatever NULL value
+plants_within_gdf = plants_within_gdf.dropna()
+print(DK_Plant_gdf.iloc(1168))
+
+for index,row in plants_within_gdf.iterrows():
+    #Check om kolonne for planten findes i DK_Plant_GDF
+    index_right = int(row['index_right'])
+    print(DK_Plant_gdf.iloc(1168))
+    print(DK_Plant_gdf.iloc(index_right))
+    if not row['species'] in DK_Plant_gdf.columns:
+        #Hvis ikke tilføj kollonen med navn = plantens navn og værdi False i alle række
+        DK_Plant_gdf[row['species']] = False
+    #find række i DK_Plant_GDF fil med index_right 
+    DK_Plant_gdf.iloc(row['index_right'])[row['species']] = True
+    #sæt værdi i kollonnen med plantens navn = True
+    #Done XD
+    if index == 10:
+        break
+    #
+#
+print(DK_Plant_gdf.head())
+#Gem oprettet geodataframe som en CSV med filnat DK_PLants_10k
+
 
 
 """
