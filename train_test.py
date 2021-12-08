c840d29abafca4e2f969229588f48adc425a9c8e
Christiane <christianekiel@hotmail.com>
2021-12-08 16:22:35 +0100

    HGS

--- train_test.py
+++ train_test.py
@@ -13,14 +13,16 @@ import csvTools
 from sklearn.model_selection import cross_val_score
 import geopandas as gpd
 from sklearn.utils import shuffle
-import seaborn as sns
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingGridSearchCV
+from sklearn.datasets import make_classification
 
 def print_confusion_matrix(y_true, y_pred):
     cm = confusion_matrix(y_true, y_pred)
-    print('True positive = ', cm[0][0])
+    print('True positive = ', cm[1][1])
     print('False positive = ', cm[0][1])
     print('False negative = ', cm[1][0])
-    print('True negative = ', cm[1][1])
+    print('True negative = ', cm[0][0])
 
 
 crs_earth = 'EPSG:4326'
@@ -33,12 +35,18 @@ crs = crs_maps
 #Dactylis glomerata <-- Hundegræs, mange oberservationer (5000+)
 #Crambe maritima <-- Strandkål få observationer (400+) Vokser kun ved kysten.
 
+#Rosa spinosissima
+#Rumex sanguineus(wood dock)
+#Urtica dioica <-- Stornælde MANGE observationer (5000+)
+#Impatiens parviflora
+#Crambe maritima <-- Strandkål få observationer (400+) Vokser kun ved kysten.
+
 
 thePlantToFind = input('Indtast en plante her: ')
 
 #plant_gdf_grid = csvTools.convert_csv_to_gdf('csv_files\\DK_Plant_10000.csv',True,crs=crs)
 #plant_gdf_grid = plant_gdf_grid.drop(columns=['geometry'])
-data = csvTools.convert_csv_to_gdf('csv_files/hallofinalcsv.csv',True,crs=crs)
+data = csvTools.convert_csv_to_gdf('/Users/christianekiel/Desktop/7. semester/Projekt/Dataset/Dataset-creator/csv_files/hallofinalcsv.csv',True,crs=crs)
 #data = data.drop(['geometry','Unnamed: 0'], axis = 1)
 #dataNoPlants = data.drop(columns=plant_gdf_grid.columns)
 
@@ -58,36 +66,56 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, rand
 new_x_train = np.delete(x_train, [0, 1, 2, 3], axis = 1)
 new_x_test  = np.delete(x_test, [0, 1 , 2, 3], axis = 1)
 
-k = 8
-kf = KFold(n_splits=k, random_state=None)
+#k = 8
+#kf = KFold(n_splits=k, random_state=None)
 
 print('x_train: ', x_train.shape)
 print('y_train: ', y_train.shape)
 print('x_test: ', x_test.shape)
 print('y_test: ', y_test.shape)
 
-rfc = RandomForestClassifier(n_estimators = 1000, random_state= 42)
+rfc = RandomForestClassifier(random_state=42)
 rfc.fit(new_x_train, y_train)
+
+
+param_grid = {"n_estimators": [100, 300, 500, 1000],
+              "max_depth": [3, 5, 7, 10, 30, 50]}
+HGS = HalvingGridSearchCV(rfc, param_grid, cv=5, random_state=42).fit(new_x_train,y_train)
+
+print(HGS.best_estimator_)
+
+
+#rfc.fit(new_x_train, y_train)
+#print(rfc.get_params)
 
 predictions = rfc.predict(new_x_test)
 probabilities = rfc.predict_proba(new_x_test)
 trueplots = []
-probabilitylist = []
+negativeplots = []
 for index, values in enumerate(predictions):
     #for probality in enumerate(probabilities):
         geo = x_test[index][3]
         prob = probabilities[index][1]
         probabilitylist.append(prob)
         trueplots.append(geo)
-
-print(trueplots)
-print(probabilitylist)            
+    else: 
+         geo = x_test[index][3]
+         negativeplots.append(geo)
 #for i in range(len(x_test)):
-#    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ \n Probability (False, True)=%s \n Prediction=%s \n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ \n" % ((probality[i]*100), (predictions[i])))
+    #print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ \n Probability (False, True)=%s \n Prediction=%s \n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ \n" % ((probality[i]*100), (predictions[i])))
+
+rfc2 = RandomForestClassifier(max_depth=7, n_estimators=500, random_state=42)
+rfc2.fit(new_x_train, y_train)
+predictions2 = rfc2.predict(new_x_test)
+
+print('NY accuracy score of training set: ', accuracy_score(y_train, rfc2.predict(new_x_train)))
+print('NY accuracy score of test set: ', accuracy_score(y_test, predictions2))
 
 
-dataplot = pd.DataFrame({'geometry':trueplots,'prediction':probabilitylist})
-dataplot.to_csv('csv_files/dataplottest2.csv')
+dataplot = pd.DataFrame({'geometry':trueplots})
+dataplot.to_csv('Dataset/Dataset-creator/csv_files/trueplots.csv')
+dataplot = pd.DataFrame({'geometry':negativeplots})
+dataplot.to_csv('Dataset/Dataset-creator/csv_files/negativeplots.csv')
 
 print(probabilities[1])
 print(x_test[3])
@@ -95,6 +123,11 @@ print(x_test[3])
 #print(allPlotsGDF)
 
 print('accuracy score of training set: ', accuracy_score(y_train, rfc.predict(new_x_train)))
+print(rfc.get_params())
+
+
+print('accuracy score of training set: ', accuracy_score(y_train, rfc.predict(new_x_train)))
+
 print('accuracy score of test set: ', accuracy_score(y_test, predictions))
 print('confusion matrix: ')
 print(confusion_matrix(y_test,predictions,labels=[1,0]))
@@ -103,7 +136,8 @@ print_confusion_matrix(y_test,predictions)
 #  print(index, values)
 
 
-#result = cross_val_score(rfc , x, y, cv = kf)
+#result = cross_val_score(rfc2 , x, y, cv = kf)
+#print(result)
 
 
 """ 
@@ -127,7 +161,7 @@ for pair in feature_importances:
 
 #print(data1.isnull().values.sum())
 
-plants = pd.read_csv('data/data_with_lat_long.csv', error_bad_lines = False, engine='python')
+plants = pd.read_csv('Dataset/Dataset-creator/data/data_with_lat_long.csv', error_bad_lines = False, engine='python')
 plantsdub = plants[(plants['species']!=thePlantToFind)].index
 plants.drop(plantsdub, inplace=True)
 
