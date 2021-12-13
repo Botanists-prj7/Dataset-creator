acac99f3c529a5d71fb65ff8779ef8ab2da860ed
Frederik Fuglsang Midtgaard <midtgaard@hotmail.dk>
2021-12-13 11:59:14 +0100

    Added functionality to print map of a dataset

--- heatMapResults.py
+++ heatMapResults.py
@@ -15,3 +15,16 @@ def print_heatmap_of_predictions(column:str, crs: str, geodataframe_csv_location
     ax = geodataframe_results.plot(column = column, legend=True, cmap='RdYlGn', scheme='User_Defined',classification_kwds=dict(bins=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]))
     cx.add_basemap(ax,source=cx.providers.CartoDB.Positron)
     plt.show()
+
+def print_map_of_dataset(crs: str, geodataframe_csv_location:str = None, geodataframe:gpd.GeoDataFrame = None):
+    if geodataframe is not None:
+        geodataframe_results = geodataframe
+    elif geodataframe_csv_location is not None:
+        geodataframe_results = csvTools.convert_csv_to_gdf(geodataframe_csv_location,True, crs)
+    else :
+        print("Couldn't printpredictions as neither a csv file or a geodataframe was supplied")
+        return None
+
+    ax = geodataframe_results.plot()
+    cx.add_basemap(ax,source=cx.providers.CartoDB.Positron)
+    plt.show()
